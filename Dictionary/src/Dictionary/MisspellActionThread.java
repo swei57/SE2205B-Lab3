b8a86057ca8502c0678ece4ee63c3205aa6a4dbf
swei57 <swei57@users.noreply.github.com>
2018-03-02 10:18:59 -0500

    Lab3 init commit

---
+++ Dictionary/src/Dictionary/MisspellActionThread.java
@@ -0,0 +1,136 @@
+package Dictionary;
+
+import java.io.*;
+import java.util.*;
+import javafx.application.Platform;
+
+/**
+ * A Thread that contains the application we are going to animate
+ *
+ */
+public class MisspellActionThread implements Runnable {
+
+    DictionaryController controller;
+    private final String textFileName;
+    private final String dictionaryFileName;
+
+    private LinesToDisplay myLines;
+    private DictionaryInterface<String, String> myDictionary;
+    private boolean dictionaryLoaded;
+
+    /**
+     * Constructor for objects of class MisspellActionThread
+     *
+     * @param controller
+     */
+    public MisspellActionThread(DictionaryController controller) {
+        super();
+
+        this.controller = controller;
+        textFileName = "check.txt";
+        dictionaryFileName = "sampleDictionary.txt";
+
+        myDictionary = new HashedMapAdaptor<String, String>();
+        myLines = new LinesToDisplay();
+        dictionaryLoaded = false;
+
+    }
+
+    @Override
+    public void run() {
+
+        // ADD CODE HERE TO LOAD DICTIONARY
+
+
+        Platform.runLater(() -> {
+            if (dictionaryLoaded) {
+               controller.SetMsg("The Dictionary has been loaded"); 
+            } else {
+               controller.SetMsg("No Dictionary is loaded"); 
+            }
+        });
+        
+        // ADD CODE HERE TO CALL checkWords
+        
+
+    }
+
+    /**
+     * Load the words into the dictionary.
+     *
+     * @param theFileName The name of the file holding the words to put in the
+     * dictionary.
+     * @param theDictionary The dictionary to load.
+     */
+    public void loadDictionary(String theFileName, DictionaryInterface<String, String> theDictionary) {
+        Scanner input;
+        try {
+            String inString;
+            String correctWord;
+
+            input = new Scanner(new File(theFileName));
+
+            // ADD CODE HERE TO READ WORDS INTO THE DICTIONARY     
+
+            
+            
+        } catch (IOException e) {
+            System.out.println("There was an error in reading or opening the file: " + theFileName);
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    /**
+     * Get the words to check, check them, then put Wordlets into myLines. When
+     * a single line has been read do an animation step to wait for the user.
+     *
+     */
+    public void checkWords(String theFileName, DictionaryInterface<String, String> theDictionary) {
+        Scanner input;
+        try {
+            String inString;
+            String aWord;
+
+            input = new Scanner(new File(theFileName));
+            // ADD CODE HERE    
+
+            
+            
+        } catch (IOException e) {
+            System.out.println("There was an error in reading or opening the file: " + theFileName);
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    /**
+     * Check the spelling of a single word.
+     *
+     */
+    public boolean checkWord(String word, DictionaryInterface<String, String> theDictionary) {
+        boolean result = false;
+
+        // ADD CODE HERE    
+
+        
+        
+
+        return result;
+
+    }
+
+    private void showLines(LinesToDisplay lines) {
+        try {
+            Thread.sleep(500);
+            Platform.runLater(() -> {
+                if (myLines != null) {
+                    controller.UpdateView(lines);
+                }
+            });
+        } catch (InterruptedException ex) {
+        }
+    }
+
+} // end class MisspellActionThread
+
